576f38cf271b67ba2bfff2dffcbc05c18e9fa769
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-21 16:31:04 +0200

    correct syntax error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/version", (req, res) => {
   res.send("9"); // change this string to ensure a new version deployed
-);
+});
 
 app.get("/health", (req, res) => {
   res.send("ok");
